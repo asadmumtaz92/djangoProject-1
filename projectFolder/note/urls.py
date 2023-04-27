31839a12bea3d3d0ca1cd2c10e19ac7168d5312d
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-04-27 17:35:41 +0500

    This commit for add link/href for navigate one to another page...

--- projectFolder/note/urls.py
+++ projectFolder/note/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.listOfAllNotes),
-    path('list/<int:pk>', views.noteDetail),
+    path('list/', views.listOfAllNotes, name="note.list"),
+    path('list/<int:pk>', views.noteDetail, name="note.detail"),
 
     # USUNG CLASS BASE
     # path('list/', views.NotesListView.as_view()),
