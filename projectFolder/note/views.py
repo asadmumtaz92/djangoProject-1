2bd586d5e4a987761e20ffed8530b3673570d9ec
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-04-27 12:32:21 +0500

    This commit for change 404 page for notes...

--- projectFolder/note/views.py
+++ projectFolder/note/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 # Create your views here.
 from .models import Notes
@@ -12,5 +12,6 @@ def noteDetail(request, pk):
     try:
         note = Notes.objects.get(pk=pk)
     except Notes.DoesNotExist:
-        raise Http404('Notes dose not exist')
+        # raise Http404('Notes dose not exist')
+        return HttpResponse('<h1 style="text-align: center; margin-top: 100px">Notes dose not exist</h1>')
     return render(request, 'note/noteDetail.html', {'note': note})
