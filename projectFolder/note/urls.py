5559af23dd21b7efd6e9a564878a74d3788d6301
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-04-27 15:22:45 +0500

    This commit for update view/url using class base...

--- projectFolder/note/urls.py
+++ projectFolder/note/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('list/', views.listOfAllNotes),
     path('list/<int:pk>', views.noteDetail),
+
+    # USUNG CLASS BASE
+    # path('list/', views.NotesListView.as_view()),
+    # path('list/<int:pk>', views.NoteDetailView.as_view()),
 ]
--- projectFolder/note/views.py
+++ projectFolder/note/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
+from django.views.generic import ListView
 
-# Create your views here.
 from .models import Notes
 
 def listOfAllNotes(request):
@@ -16,3 +16,16 @@ def noteDetail(request, pk):
         # raise Http404('Notes dose not exist')
         return HttpResponse('<h1 style="text-align: center; margin-top: 100px">Notes dose not exist</h1>')
     return render(request, 'note/noteDetail.html', {'note': note})
+
+
+# USUNG CLASS BASE
+
+# class NotesListView(ListView):
+#     model = Notes
+#     context_object_name = "allNotes"
+#     template_name = 'note/noteList.html'
+
+# class NoteDetailView(ListView):
+#     model = Notes
+#     context_object_name = "note"
+#     template_name = 'note/noteDetail.html'
